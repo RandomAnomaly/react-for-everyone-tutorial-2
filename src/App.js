f4f26ec7182e317ba9800a269fd9fb7020e550b3
jack <jack.gammon@equinox.co.nz>
2016-08-08 12:15:34 +1200

    Completed lesson 11

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ContactsList from './contactsList';
 import {render} from 'react-dom';
 
+let contacts = {
+	name: 'Scott',
+	phone: '555 333 5555'
+}
+
+
 class App extends React.Component {
 	render(){
 		return (
 			<div>
 				<h1>Contacts List</h1>
-				<ContactsList />
+				<ContactsList contacts={this.props.contacts} />
 			</div>
 		)
 	}	
 }
 
-render(<App />, document.getElementById("app"));
+render(<App contacts={contacts} />, document.getElementById("app"));

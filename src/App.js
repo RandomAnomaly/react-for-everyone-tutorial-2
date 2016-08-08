615784b840dbaa91f168f2aa2f0e347f64f4b074
jack <jack.gammon@equinox.co.nz>
2016-08-08 12:27:51 +1200

    Completed lesson-12

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ContactsList from './contactsList';
 import {render} from 'react-dom';
 
-let contacts = {
+let contacts = [{
+	id: 1,
 	name: 'Scott',
 	phone: '555 333 5555'
-}
+}, {
+	id: 2,
+	name: 'Courtney',
+	phone: '555 111 5555'
+}, {
+	id: 3,
+	name: 'Tim',
+	phone: '111 333 5555'
+}, {
+	id: 4,
+	name: 'Jeff',
+	phone: '123 456 7891'
+}]
 
 
 class App extends React.Component {
